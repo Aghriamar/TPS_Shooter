3c93c11bb2aa46ffee13a97a7b09d62a739e969c
Aghriamar <A1exXx88@inbox.ru>
2025-02-12 22:57:30 +0300

    Lesson_02

--- Source/TPS_Shooter/Game/TPS_ShooterGameMode.cpp
+++ Source/TPS_Shooter/Game/TPS_ShooterGameMode.cpp
@@ -11,7 +11,7 @@ ATPS_ShooterGameMode::ATPS_ShooterGameMode()
 	PlayerControllerClass = ATPS_ShooterPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprint/Character/TopDownCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprint/Character/BP_Character"));
 	if (PlayerPawnBPClass.Class != nullptr)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
