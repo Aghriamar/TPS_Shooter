9a120314d3581580db8418ef08638611247f2ed9
Aghriamar <A1exXx88@inbox.ru>
2025-03-01 18:59:33 +0300

    Lesson_04

--- Source/TPS_Shooter/Character/TPS_ShooterCharacter.cpp
+++ Source/TPS_Shooter/Character/TPS_ShooterCharacter.cpp
@@ -46,7 +46,7 @@ ATPS_ShooterCharacter::ATPS_ShooterCharacter()
 	// Create a decal in the world to show the cursor's location
 	CursorToWorld = CreateDefaultSubobject<UDecalComponent>("CursorToWorld");
 	CursorToWorld->SetupAttachment(RootComponent);
-	static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/Blueprint/Character/M_Cursor_Decal.M_Cursor_Decal'"));
+	static ConstructorHelpers::FObjectFinder<UMaterial> DecalMaterialAsset(TEXT("Material'/Game/Materials/M_Cursor_Decal.M_Cursor_Decal'"));
 	if (DecalMaterialAsset.Succeeded())
 	{
 		CursorToWorld->SetDecalMaterial(DecalMaterialAsset.Object);
